cb5fd6d9d37bf9d6309dbe186346f03ec46ae8ce
ilkayseki <58669159+ilkayseki@users.noreply.github.com>
2024-05-22 19:03:39 +0300

    Init - Assets Added
    All assets and packages added init

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    private bool isMoving = true;
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Stop()
+    {
+        isMoving = false;
+    }
+}
